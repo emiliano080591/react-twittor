51680012a23ff0d92c0aa9656cb1c738a371e40e
Kratos <emilianoruben91@gmail.com>
2020-07-24 18:28:14 -0500

    Se agregaron modales en la pagina de login y registro

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React,{useState} from 'react';
-
+import SignInSignUp from './pages/SignInSignUp';
 
 export default function App() {
   const [user,setUser]=useState({name:"Emiliano"});
-
   return(
     <div>
-      {user ? <h1>Estas logeado</h1> : <h1>No estas logeado</h1>};
+      {user ? (
+        <div>
+          <SignInSignUp/>
+        </div>
+      ) : (
+        <h1>No estas logeado</h1>)}
     </div>
   )
+
 }
---
+++ src/pages/SignInSignUp/SignInSignUp.js
@@ -0,0 +1,72 @@
+import React,{useState} from 'react';
+import {Container,Row,Col,Button} from 'react-bootstrap';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faSearch,faUsers,faComment} from '@fortawesome/free-solid-svg-icons';
+import BasicModal from '../../components/Modal/BasicModal';
+import LogoWhiteTwittor from '../../assets/png/logo-white.png';
+import LogoTwittor from '../../assets/png/logo.png';
+import "./SignInSignUp.scss";
+
+export default function SignInSignUp() {
+    const [showModal, setShowModal] = useState(false);
+    const [contentModal, setContentModal] = useState(null);
+
+    const openModal=content=>{
+        setShowModal(true);
+        setContentModal(content);
+    };
+
+    return (
+        <>
+        <Container className="signin-signup" fluid>
+            <Row>
+                <LeftComponent/>
+                <RightComponent
+                    openModal={openModal}
+                    setShowModal={setShowModal}
+                />
+            </Row>
+        </Container>
+        <BasicModal show={showModal} setShow={setShowModal}>
+            {contentModal}
+        </BasicModal>
+        </>
+    )
+}
+
+function LeftComponent(){
+    return (
+        <Col className="signin-signup__left" xs="6">
+            <img src={LogoTwittor} alt="twittor"/>
+            <div>
+                <h2>
+                    <FontAwesomeIcon icon={faSearch}/>
+                    Sigue lo que te interesa
+                </h2>
+                <h2>
+                    <FontAwesomeIcon icon={faUsers}/>
+                    Enterate de qué esté hablando la gente.</h2>
+                <h2>
+                    <FontAwesomeIcon icon={faComment}/>
+                    Unete a la conversación.</h2>
+            </div>
+        </Col>
+    )
+}
+
+function RightComponent(props){
+    const {openModal,setShowModal}=props;
+    return (
+        <Col className="signin-signup__right" xs="6">
+            <div>
+                <img src={LogoWhiteTwittor} alt="twittorWhite"/>
+                <h2>
+                    Mira lo que esta pasando en el mundo en este momento
+                </h2>
+                <h3>Unete a twittor hoy mismo.</h3>
+                <Button onClick={()=>openModal(<h2>Formulario de registro</h2>)} variant="primary">Registrate</Button>
+                <Button onClick={()=>openModal(<h2>Formulario de login</h2>)} variant="outline-primary">Iniciar Sesión</Button>
+            </div>
+        </Col>
+    )
+}
